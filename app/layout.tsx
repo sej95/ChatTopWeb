95b7d7414219864525f22d60358df52dcfd3fb5f
sej95 <144600333+sej95@users.noreply.github.com>
2024-11-27 22:52:01 +0800

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,25 +3,29 @@ import "./styles/globals.scss";
 import "./styles/markdown.scss";
 import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
-import { type Metadata } from "next";
+import type { Metadata, Viewport } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { getServerSideConfig } from "./config/server";
+import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
+const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "Gemini Chat Pro - Your personal Gemini Pro Chat Bot",
-  description:
-    "Gemini-Next-Web is the best open source and free chat bot powered by Google Gemini Pro and Gemini Pro Vision models. You can use it to chat with texts and images. All the data are stored in local and there is no need to register an account.",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
+  title: "NextChat",
+  description: "Your personal ChatGPT Chat Bot.",
+  appleWebApp: {
+    title: "NextChat",
+    statusBarStyle: "default",
   },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "#fafafa" },
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
-  appleWebApp: {
-    title: "Gemini Chat Pro",
-    statusBarStyle: "default",
-  },
 };
 
 export default function RootLayout({
@@ -33,10 +37,35 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="config" content={JSON.stringify(getClientConfig())} />
-        <link rel="manifest" href="/site.webmanifest"></link>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <link
+          rel="manifest"
+          href="/site.webmanifest"
+          crossOrigin="use-credentials"
+        ></link>
         <script src="/serviceWorkerRegister.js" defer></script>
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        {serverConfig?.isVercel && (
+          <>
+            <SpeedInsights />
+          </>
+        )}
+        {serverConfig?.gtmId && (
+          <>
+            <GoogleTagManager gtmId={serverConfig.gtmId} />
+          </>
+        )}
+        {serverConfig?.gaId && (
+          <>
+            <GoogleAnalytics gaId={serverConfig.gaId} />
+          </>
+        )}
+      </body>
     </html>
   );
 }
